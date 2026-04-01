ad9fdf174c4b5db676ce4d652b9a14f8601d7e58
Thanapon Johdee <66236295+yokeTH@users.noreply.github.com>
2026-04-01 19:24:29 +0700

    feat: theme

--- web/app/dashboard/layout.tsx
+++ web/app/dashboard/layout.tsx
@@ -3,6 +3,7 @@ import { BreadcrumbProvider } from "@/contexts/breadcrumb-context";
 import { DynamicBreadcrumb } from "@/components/dynamic-breadcrumb";
 import { SidebarTrigger } from "@/components/ui/sidebar";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
+import { ThemeToggle } from "@/components/theme-toggle";
 
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
   return (
@@ -10,9 +11,14 @@ export default function DashboardLayout({ children }: { children: React.ReactNod
       <SidebarProvider>
         <AppSidebar />
         <SidebarInset>
-          <header className="flex h-16 shrink-0 items-center gap-2 px-4 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
-            <SidebarTrigger className="hidden max-md:block" />
-            <DynamicBreadcrumb />
+          <header className="flex h-16 shrink-0 items-center gap-2 px-4 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12 justify-between">
+            <div>
+              <SidebarTrigger className="hidden max-md:block" />
+              <DynamicBreadcrumb />
+            </div>
+            <div>
+              <ThemeToggle />
+            </div>
           </header>
           <div className="flex flex-1 flex-col gap-4 p-10 pt-0">{children}</div>
         </SidebarInset>
